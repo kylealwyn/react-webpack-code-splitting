89dfe66401b1ed6472e9245e55fb5242ced8fe0c
Kyle Alwyn <kyle@coverhound.com>
2017-05-18 14:46:42 -0700

    Add import syntax babel plugin

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import { Router, Route, hashHistory } from 'react-router';
 
-const loadRoute = cb => module => cb(null, module.default);
+const curryLoader = cb => module => cb(null, module.default || module);
 
 export default () => (
   <Router history={hashHistory}>
     <Route path='/' getComponent={(nextState, cb) => {
-      require(['./home'], loadRoute(cb))
+      import('./home').then(curryLoader(cb))
     }} />
 
     <Route path='/projects' getComponent={(nextState, cb) => {
-      require(['./projects'], loadRoute(cb))
+      import('./projects').then(curryLoader(cb))
     }} />
 
     <Route path='/about' getComponent={(nextState, cb) => {
-      require(['./about'], loadRoute(cb))
+      import('./about').then(curryLoader(cb))
     }} />
   </Router>
 )
